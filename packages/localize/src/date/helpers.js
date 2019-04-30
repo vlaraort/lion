911da1eb04c5257c7e05641a60c882765218ddc3
Thomas Allmer <thomas.allmer@ing.com>
2019-04-30 14:24:36 +0200

    chore: calendar header

--- packages/localize/src/date/helpers.js
+++ packages/localize/src/date/helpers.js
@@ -10,3 +10,18 @@ export function isSameDay(day1, day2) {
     day1.getFullYear() === day2.getFullYear()
   );
 }
+
+export function getFirstDayNextMonth(date) {
+  const result = new Date(date);
+  result.setDate(1);
+  result.setMonth(date.getMonth() + 1);
+  return result;
+}
+
+export function getLastDayPreviousMonth(date) {
+  const prev = new Date(date);
+  prev.setDate(1);
+  prev.setMonth(date.getMonth() - 1);
+
+  return new Date(prev.getFullYear(), prev.getMonth(), 0);
+}
