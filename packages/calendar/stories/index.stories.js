c53dcc310ec5ff06860e63ffa0307ad9df7f5b2e
gerjanvangeest <Gerjan.van.Geest@ing.com>
2019-05-07 17:11:13 +0200

    chore(calendar): extend demo stories

--- packages/calendar/stories/index.stories.js
+++ packages/calendar/stories/index.stories.js
@@ -1,36 +1,85 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
+import { css } from '@lion/core';
 
 import '../lion-calendar.js';
+
+const calendarDemoStyle = css`
+  .demo-calendar {
+    border: 1px solid #adadad;
+    box-shadow: 0 0 16px #ccc;
+    max-width: 500px;
+  }
+`;
 
 storiesOf('Calendar|Standalone', module)
   .add(
     'default',
     () => html`
       <style>
-        .calendar {
-          border: 1px solid #adadad;
-          box-shadow: 0 0 16px #ccc;
-          max-width: 500px;
-        }
+        ${calendarDemoStyle}
       </style>
 
-      <lion-calendar class="calendar"></lion-calendar>
+      <lion-calendar class="demo-calendar"></lion-calendar>
+    `,
+  )
+  .add(
+    'selectedDate',
+    () => html`
+      <style>
+        ${calendarDemoStyle}
+      </style>
+
+      <lion-calendar
+        class="demo-calendar"
+        .selectedDate="${new Date('2019/12/09')}"
+      ></lion-calendar>
+    `,
+  )
+  .add(
+    'minDate',
+    () => html`
+      <style>
+        ${calendarDemoStyle}
+      </style>
+
+      <lion-calendar class="demo-calendar" .minDate="${new Date('2019/12/09')}"></lion-calendar>
+    `,
+  )
+  .add(
+    'maxDate',
+    () => html`
+      <style>
+        ${calendarDemoStyle}
+      </style>
+
+      <lion-calendar class="demo-calendar" .maxDate="${new Date('2019/12/09')}"></lion-calendar>
     `,
   )
   .add(
     'disabled Sa, Su',
     () => html`
       <style>
-        .calendar {
-          border: 1px solid #adadad;
-          box-shadow: 0 0 16px #ccc;
-          max-width: 500px;
-        }
+        ${calendarDemoStyle}
       </style>
 
       <lion-calendar
-        class="calendar"
+        class="demo-calendar"
         .enabledDates=${day => day.getDay() !== 6 && day.getDay() !== 0}
+      ></lion-calendar>
+    `,
+  )
+  .add(
+    'combined disabled dates',
+    () => html`
+      <style>
+        ${calendarDemoStyle}
+      </style>
+
+      <lion-calendar
+        class="demo-calendar"
+        .enabledDates=${day => day.getDay() !== 6 && day.getDay() !== 0}
+        .minDate="${new Date()}"
+        .maxDate="${new Date('2019/12/09')}"
       ></lion-calendar>
     `,
   );
